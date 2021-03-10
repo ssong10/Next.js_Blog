cc5a86b2e9c85b497daf4ecc8e8c545859c9806b
ssong10 <leesy1403@naver.com>
2021-03-10 23:47:31 +0900

    markdown detail

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Layout from '../components/layout'
 import Markdown from '../components/markdown'
 import { mdParser } from '../lib/posts';
-
+import Head from 'next/head'
 export default function Home({data}) {
   return (
     <Layout>
+      <Head>
+        <title>Ssong10</title>
+      </Head>
       <main>
         <Markdown data={data}/>
       </main>
