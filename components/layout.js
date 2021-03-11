bf075babebccd926c1c3b894724e906deaa87e84
ssong10 <leesy1403@naver.com>
2021-03-11 16:34:30 +0900

    add posts

--- components/layout.js
+++ components/layout.js
@@ -19,6 +19,7 @@ export default function Layout({children}) {
           </a>
         </div>
         <div><small>Ssong10</small></div>
+        <div><small>leesy1403@naver.com</small></div>
       </footer>
     </>
   )
